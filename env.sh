36cadfd3cb465a8969360ef9e761e1c237e25b5d
Sandeep Gunduboyina <sandeepgy11@gmail.com>
2016-11-25 03:21:00 +0530

    Remove zsh theme

--- env.sh
+++ env.sh
@@ -8,12 +8,6 @@ export PATH="~/.rbenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/loca
 export ANDROID_HOME="/usr/local/opt/android-sdk"
 export EDITOR='subl -w'
 
-# Set name of the theme to load.
-# Look in ~/.oh-my-zsh/themes/
-# Optionally, if you set this to "random", it'll load a random theme each
-# time that oh-my-zsh is loaded.
-ZSH_THEME="pygmalion"
-
 # FileSearch
 function f() { find . -iname "*$1*" ${@:2} }
 function r() { grep "$1" ${@:2} -R . }
